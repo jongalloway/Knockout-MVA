24b4eaf170dee7c8f097548ab35ca1bd21a95413
Christopher Harrison <chrhar@microsoft.com>
2015-09-08 14:58:11 -0700

    Update

--- Knockout MVA Demos/M4-BugTracker/Scripts/appViewModel.js
+++
@@ -1,8 +0,0 @@
-﻿define(['knockout-3.3.0'], function (ko) {
-	return function appViewModel() {
-		this.firstName = ko.observable('Bert');
-		this.firstNameCaps = ko.pureComputed(function () {
-			return this.firstName().toUpperCase();
-		}, this);
-	};
-});
