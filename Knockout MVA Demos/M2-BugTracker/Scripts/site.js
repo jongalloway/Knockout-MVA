729f5d8fa900559c24e808a6d48a9ad1581a8118
Christopher Harrison <chrhar@microsoft.com>
2015-09-08 11:15:36 -0700

    Module 2 slides and demos

---
+++ Knockout MVA Demos/M2-BugTracker/Scripts/site.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="models.js" />
+
+var AppViewModel = function () {
+	var self = this;
+
+	self.developers = ko.observableArray([]);
+	self.bugs = ko.observableArray([]);
+
+	self.isModifyDeveloper = false;
+	self.currentDeveloper = ko.observable(new Developer());
+	self.saveDeveloper = function () {
+		// add developer
+		if(!self.isModifyDeveloper) self.developers.unshift(self.currentDeveloper());
+		// reset developer
+		self.currentDeveloper(new Developer());
+		self.isModifyDeveloper = false;
+	}
+	self.modifyDeveloper = function(developer) {
+		// developer will automatically be the one the user clicked on
+		self.currentDeveloper(developer);
+		self.isModifyDeveloper = true;
+	}
+
+	self.currentBug = ko.observable(new Bug());
+	self.saveBug = function () {
+		self.bugs.unshift(self.currentBug());
+		self.currentBug(new Bug());
+	}
+}
+
+// Bind up the view model
+ko.applyBindings(new AppViewModel());
---
+++ Knockout MVA Demos/Mod1-BugTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mod1_BugTracker.WebApiApplication" Language="C#" %>
